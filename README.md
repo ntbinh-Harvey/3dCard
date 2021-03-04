fcc4165bd479b69803fb14bc1beb1d9cbaff1013
ntbinh6 <ntbinh6@cmc.com.vn>
2021-03-04 12:13:34 +0700

    feat: Initialize 3d Card

---
+++ README.md
@@ -0,0 +1,5 @@
+Trying some stuff in js
+
+
+https://user-images.githubusercontent.com/57211574/110199460-04bd4980-7e8b-11eb-9768-1bab454be99e.mp4
+
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Movement animation to happen
 const card = document.querySelector('.card')
 const container = document.querySelector('.container')
+const title = document.querySelector('.title')
+const sneaker = document.querySelector('.sneaker img')
+const purchase = document.querySelector('.purchase button')
+const description = document.querySelector('.info h3')
+const sizes = document.querySelector('.sizes')
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
@@ -12,10 +17,20 @@ container.addEventListener("mousemove", (e) => {
 //Animate In
 container.addEventListener('mouseenter', e => {
     card.style.transform = "none"
+    title.style.transform = "translateZ(150px)"
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)"
+    description.style.transform = "translateZ(150px)"
+    sizes.style.transform = "translateZ(150px)"
+    purchase.style.transform = "translateZ(150px)"
 })
 
 //Animate Out
 container.addEventListener('mouseleave', e => {
     card.style.transition = "all 0.5s ease"
     card.style.transform = `rotateY(0deg) rotateX(0deg)`
+    title.style.transform = "translateZ(0px)"
+    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)"
+    description.style.transform = "translateZ(0px)"
+    sizes.style.transform = "translateZ(0px)"
+    purchase.style.transform = "translateZ(0px)"
 })
